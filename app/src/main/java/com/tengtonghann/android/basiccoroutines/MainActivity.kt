c1f9716f16fce41da4b3a2f2536445c8ddf145be
Tonghann Teng <tengtonghann@gmail.com>
2020-02-03 20:03:38 -0600

    Basic CoroutineScope

--- app/src/main/java/com/tengtonghann/android/basiccoroutines/MainActivity.kt
+++ app/src/main/java/com/tengtonghann/android/basiccoroutines/MainActivity.kt
@@ -2,11 +2,29 @@ package com.tengtonghann.android.basiccoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
+    companion object {
+        const val TAG = "MainActivityLog"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.d(TAG, "Hello: ${Thread.currentThread().name}")
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d(TAG, "Hello: ${Thread.currentThread().name}")
+        }
     }
 }
